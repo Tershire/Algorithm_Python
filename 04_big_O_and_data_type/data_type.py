1c2ec04af5bcc36791adda9f3a96b58bf77cee49
tershire <tershire@gmail.com>
2023-09-19 09:11:10 +0200

    done chapter 4

---
+++ 04_big_O_and_data_type/data_type.py
@@ -0,0 +1,48 @@
+# data_type.py
+import copy
+
+# Tershire
+# 2023 SEP 19
+
+
+# set /////////////////////////////////////////////////////////////////////////
+a = set()
+print(a)
+print(type(a))
+
+b = {3, 1, 2, 2}
+print(b)
+print(type(b))
+
+# sequence ////////////////////////////////////////////////////////////////////
+# str =========================================================================
+a = "apple"
+print(id(a))
+a = "banana"
+print(id(a))
+a = "apple"
+print(id(a))
+
+# immutable ///////////////////////////////////////////////////////////////////
+a = 10
+print(id(a))
+b = a
+print(id(a))
+a += 1
+print(a, b)
+print(id(a), id(b))
+a = 10
+print(id(a), id(b))
+c = 10
+print(id(c))
+
+# is and == ///////////////////////////////////////////////////////////////////
+a = [3, 1, 2]
+b = [3, 1, 2]
+print(id(a), id(b))
+b = a
+print(id(a), id(b))
+b = list(a)
+print(a is b, a == b)
+b = copy.deepcopy(a)
+print(a is b, a == b)
