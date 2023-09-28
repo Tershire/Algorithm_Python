2e84f5cdd510073fdb69e77463149c5292897df3
tershire <tershire@gmail.com>
2023-09-28 11:11:39 +0200

    added re and max tips.

--- 06_string_manipulation/key_takeaways.py
+++ 06_string_manipulation/key_takeaways.py
@@ -4,6 +4,10 @@
 # 2023 SEP 27
 
 
+import re
+import collections
+
+
 # string | list reverse ///////////////////////////////////////////////////////
 a = "abcde"
 # check simple palindrome
@@ -33,3 +37,31 @@ print(logs)
 # <M2> sort key & lambda function
 logs.sort(key=lambda x: (x.split()[1:], x.split()[0]))
 print(logs)
+
+
+# re (regular expression) /////////////////////////////////////////////////////
+# how to split a string with multiple delimiters:
+paragraph = "Hi. My name* is, well;, 'HAPPY'?!"
+
+# <M1> if the list of delimiters given
+print(re.sub("[!?',;.]", " ", paragraph.lower()))
+
+# <M2> if not given
+#  ^: means "except"
+# \w: means [a-zA-Z0-9_]
+print(re.sub("[^\w]", " ", paragraph.lower()))
+
+
+# max: usage with dict ////////////////////////////////////////////////////////
+# how to find the key for the max value in a dict
+letters = ["a", "b", "b", "b", "c", "c"]
+counts = collections.Counter(letters)
+print(counts)
+
+# <M1> max on dict
+max_key = max(counts, key=counts.get)
+print(max_key, counts[max_key])
+
+# <M2> .most_common() (Counter)
+max_items = counts.most_common(1)
+print(max_items)
