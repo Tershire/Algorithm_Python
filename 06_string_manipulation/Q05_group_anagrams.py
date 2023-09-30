7f461eb9604839be2bc68d63b5d4eba3f229a431
tershire <tershire@gmail.com>
2023-09-30 21:52:26 +0200

    done group_anagrams.

---
+++ 06_string_manipulation/Q05_group_anagrams.py
@@ -0,0 +1,28 @@
+# Q05_group_anagrams.py
+# LeetCode #49
+
+# Tershire
+# 2023 SEP 30
+
+
+import collections
+
+
+# case ////////////////////////////////////////////////////////////////////////
+strs = ["eat","tea","tan","ate","nat","bat"]
+
+
+# algo ////////////////////////////////////////////////////////////////////////
+# -----------------------------------------------------------------------------
+# defaultdict
+# 95 [ms]
+def f1(strs: list[str]) -> list[list[str]]:
+    roster = collections.defaultdict(list[str])
+    for text in strs:
+        roster["".join(sorted(text))].append(text)
+
+    return list(roster.values())
+
+
+# test ////////////////////////////////////////////////////////////////////////
+print(f1(strs))
